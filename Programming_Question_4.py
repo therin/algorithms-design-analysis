84374401c8d4b1a793a45f97ecf0ee069526df19
Sergey Borodich <sergey.borodich@xero.com>
2017-10-27 13:25:12 +1300

    graph to pickle object

--- Programming_Question_4.py
+++ Programming_Question_4.py
@@ -1,59 +1,37 @@
-import random
-import copy
+from collections import defaultdict
+import pickle
 # -*- coding: utf-8 -*-
 '''
-
 No recursion due to depth limit
-
 Use defaultdict to store linked list, instead of list, use set
-
 no need to use class for vertices to add properties (discover time, finish time, color, etc), use dict
-
 append no insert
+
+The file contains the edges of a directed graph. Vertices are labeled as positive integers from 1 to 875714. Every row indicates an edge, the vertex label in first column is the tail and the vertex label in second column is the head (recall the graph is directed, and the edges are directed from the first column vertex to the second column vertex). So for example, the 11th row looks liks : "2 47646". This just means that the vertex with label 2 has an outgoing edge to the vertex with label 47646
+
+Your task is to code up the algorithm from the video lectures for computing strongly connected components (SCCs), and to run this algorithm on the given graph.
+
+Output Format: You should output the sizes of the 5 largest SCCs in the given graph, in decreasing order of sizes, separated by commas (avoid any spaces). So if your algorithm computes the sizes of the five largest SCCs to be 500, 400, 300, 200 and 100, then your answer should be "500,400,300,200,100" (without the quotes). If your algorithm finds less than 5 SCCs, then write 0 for the remaining terms. Thus, if your algorithm computes only 3 SCCs whose sizes are 400, 300, and 100, then your answer should be "400,300,100,0,0" (without the quotes). (Note also that your answer should not have any spaces in it.)
+
+WARNING: This is the most challenging programming assignment of the course. Because of the size of the graph you may have to manage memory carefully. The best way to do this depends on your programming language and environment, and we strongly suggest that you exchange tips for doing this on the discussion forums.
 '''
 
-class Node():
-
-    def __init__(self, ida):
-        self.ida = ida
-        self.edges = []
-
-
-class Graph():
-
-    def __init__(self, nodes=None):
-        self.status = "cool"
-        self.nodes = []
-
-    def addEdge(self, node1, node2):
-        node1.edges.append(node2)
-
-    def addNode(self, ida):
-        if ida not in self.getNodesIds():
-            new_node = Node(ida)
-            self.nodes.append(new_node)
-
-    def chooseRandomEdge(self):
-        one = random.choice(self.nodes)
-        two = random.choice(one.edges)
-        return one, two
-
-    def getNodesIds(self):
-        return [node.ida for node in self.nodes]
-
-    def getNodeById(self, id):
-        for node in self.nodes:
-            if node.ida == id:
-                return node
-
-
+# Load graph from file
 def generateGraph():
-    with open('kargerMinCut.txt') as file:
-        graph = Graph()
+    with open('SCC.txt') as file:
+        graph = defaultdict(list)
         for line in file:
-            graph.addNode(line.strip().split('\t')[0])
-            for i in line.strip().split('\t')[1:]:
-                graph.addNode(i)
-                graph.addEdge(graph.getNodeById(line.strip().split('\t')[0]), graph.getNodeById(i))
+            # print(line.strip().split(' '))
+            graph[line.strip().split(' ')[0]].append(line.strip().split(' ')[1])
     return graph
+
+# Serialaze graph object to file for faster load time
+# graph = generateGraph()
+# with open('graph.pickle', 'wb') as handle:
+#     pickle.dump(graph, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+with open('graph.pickle', 'rb') as handle:
+    fast = pickle.load(handle)
+
+print(fast['11'])
 
